741205921dc168d42822ac42255315c3ee8c60eb
Arkadeep317 <arkadeepsi2007@gmail.com>
2026-07-13 01:50:33 +0530

    delete for rework

--- musicgallery/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path ,include
-from . import views 
-
-urlpatterns = [
-    # The empty string '' means this is the root of the gallery app
-    path('', views.home),
-]
--- musicgallery/myapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render,redirect
-# Create your views here.
-from django.shortcuts import render
-def home(request):
-    return render(request,"home.html")
